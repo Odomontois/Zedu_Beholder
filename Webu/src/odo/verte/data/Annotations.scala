d6f637af48019e7c26540a677393aba2d78fae43
Oleg NIzhnik <odomontois@gmail.com>
2012-05-22 03:07:18 +0400

    DB в отдельном файле

--- Webu/src/odo/verte/data/Annotations.scala
+++ Webu/src/odo/verte/data/Annotations.scala
@@ -1,10 +1,6 @@
 package odo.verte.data
 
 import annotation.target.field
-import collection.JavaConversions._
-import javax.persistence.{Query, Persistence}
-import odo.verte.serv.tree.DBException
-import com.google.appengine.api.datastore.{KeyFactory, Key}
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,21 +15,4 @@ object Annotations {
   type GeneratedValue = javax.persistence.GeneratedValue@field
 }
 
-object DB {
-  val EMF = Persistence.createEntityManagerFactory("transactions-optional")
 
-  def manager = EMF.createEntityManager()
-
-  def loadResult[T](query: Query): List[T] = {
-    val results = collectionAsScalaIterable(query.getResultList).toList
-    for (result <- results) yield result match {
-      case appropriate: T => appropriate
-      case other => throw new DBException
-    }
-  }
-
-  def key(string: String): Key = string match {
-    case "" => null
-    case some => KeyFactory.stringToKey(some)
-  }
-}
